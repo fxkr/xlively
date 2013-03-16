7899ef278b633b02fb89cf4a64a7f6cade281619
Felix Kaiser <felix.kaiser@fxkr.net>
2013-03-16 21:47:45 +0100

    Add a benchmark tool

--- Makefile
+++ Makefile
@@ -3,16 +3,19 @@ CFLAGS=-std=c99 -Wall
 LIBS=-lm -lX11 -lpthread -lpopt
 
 DEPS=
-OBJ=main.o config.o model.o view.o ctrl.o
+OBJ=config.o model.o view.o ctrl.o
 
 %.o: %.c %.h $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-xlively: $(OBJ)
+xlively: main.o $(OBJ)
+	gcc -o $@ $^ $(CFLAGS) $(LIBS)
+
+benchmark: benchmark.o $(OBJ)
 	gcc -o $@ $^ $(CFLAGS) $(LIBS)
 
 .PHONY: clean
 
 clean:
-	rm -f *.o *~ core xlively
+	rm -f *.o *~ core xlively benchmark
 
---
+++ benchmark.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+#include "config.h"
+#include "model.h"
+#include "view.h"
+#include "ctrl.h"
+
+
+int main(int argc, const char *argv[]) {
+    config_init(argc, argv);
+    model_init();
+    model_acquire_mutex();
+
+    model_configure(2048, 1024);
+    model_populate_randomly(0.2);
+
+    for (int i = 0; i < 1000; i++) {
+        model_evolve();
+    }
+
+    model_release_mutex();
+    model_exit();
+    return 0;
+}
+
